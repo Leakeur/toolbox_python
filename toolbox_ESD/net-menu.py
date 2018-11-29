7278222b1da2f948f836bac4cb049b63612808f2
leakeur <mick.descamps@gmail.com>
2018-11-29 15:22:55 +0100

    blabla

--- toolbox_ESD/net-menu.py
+++ toolbox_ESD/net-menu.py
@@ -2,7 +2,7 @@ import os
 from netconfig_tools import *
 
 
-net_list = list_net_card()
+
 
 print("*******************************************")
 print("                Make your choice           ")
@@ -16,24 +16,5 @@ print("7) Disable Anon Surf                       ")
 print("8) Reset   Anon Surf                       ")
 print("*******************************************")
 
-def launch_menu():        
-    read_cmd = input()
-    if read_cmd == 1:
-        up_down_card(net_list)
-    elif read_cmd == 2:
-        set_ip(net_list)
-    elif read_cmd == 3:
-        show_ifconfig()
-    elif read_cmd == 4:
-        change_mac(net_list)   
-    elif read_cmd == 5:
-        get_pub_ip()
-    elif read_cmd == 6:
-        enable_anon()    
-    elif read_cmd == 7:
-        disable_anon()
-    elif read_cmd == 7:
-        reset_anon()            
-    else:
-        print("Bad number")
-        launch_menu()
+launch_menu()
+
